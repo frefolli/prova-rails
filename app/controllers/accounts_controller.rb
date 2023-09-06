67b8de4cf24c2e0b9f9f7cdcb3003e402269e75b
Francesco Refolli <francesco.refolli@binarysystem.eu>
2023-09-06 15:32:41 +0200

    account controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,14 @@
 class AccountsController < ApplicationController
+  def index
+    @error = (params.has_key?(:error) ? params[:id] : "")
+    @accounts = Account.all
+  end
+
+  def show
+    @account = Account.find(params[:id])
+    if @account.nil?
+      redirect_to :accounts, :error => "account doesn't exists"
+      return
+    end
+  end
 end
