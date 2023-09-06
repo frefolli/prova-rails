00aa4b9c024a99353bce6fb8ddda18c5f184ab37
Francesco Refolli <francesco.refolli@binarysystem.eu>
2023-09-06 16:15:37 +0200

    finish account index

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,25 @@
 class AccountsController < ApplicationController
   def index
-    @error = (params.has_key?(:error) ? params[:id] : "")
+    @error = (params.has_key?(:error) ? params[:id] : nil)
     @accounts = Account.all
+
+    if params.has_key?(:commit)
+      if params[:commit] == "filter"    
+        if params.has_key?(:name) and not params[:name].empty?
+          @accounts = @accounts.where(name: params[:name])
+        end
+        
+        if params.has_key?(:surname) and not params[:surname].empty?
+          @accounts = @accounts.where(surname: params[:surname])
+        end
+        
+        if params.has_key?(:fiscalcode) and not params[:fiscalcode].empty?
+          @accounts = @accounts.where(fiscalcode: params[:fiscalcode])
+        end
+      elsif params[:commit] == "clean"
+        redirect_to :accounts
+      end
+    end
   end
 
   def show
