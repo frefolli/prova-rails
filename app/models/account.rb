342fe5eeb2647c03835e2f78f92f1da2e074575b
Francesco Refolli <francesco.refolli@binarysystem.eu>
2023-09-06 15:17:11 +0200

    validations in model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,8 @@
 class Account < ApplicationRecord
+  validates :name, length: {minimum: 1}
+  validates :surname, length: {minimum: 1}
+  validates :credit, numericality: {only_integer: true,
+                                    greater_than_or_equal_to: 0}
+  validates :name, :surname, uniqueness: true
+  validates :name, :surname, :credit, presence: true
 end
