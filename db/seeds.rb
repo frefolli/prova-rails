8b7a95876bd94529140924779f86c3756893a208
Francesco Refolli <francesco.refolli@binarysystem.eu>
2023-09-06 15:20:46 +0200

    seeds for accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+def assert_count(obj, count)
+  if obj.count() != count
+    raise obj.to_s + "::ERR: Expected allocation of " + count.to_s +  " rows, got " + obj.count().to_s
+  else
+    puts obj.to_s + "::OK"
+  end
+end
+
+def assert_create(obj, params)
+  obj.create(params)
+  assert_count(obj, params.length)
+end
+
+Account.delete_all
+
+assert_create(Account, [
+  {:name => "a", :surname => "A", :credit => 0},
+  {:name => "b", :surname => "B", :credit => 0},
+  {:name => "c", :surname => "C", :credit => 0},
+  {:name => "d", :surname => "D", :credit => 0},
+  {:name => "a", :surname => "E", :credit => 0},# same name    OK
+  {:name => "e", :surname => "A", :credit => 0} # same surname OK
+  {:name => "a", :surname => "A", :credit => 0} # full replica ERR
+])
